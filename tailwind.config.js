3e5a621c6251e8d77f367d57ca731841f4205f62
unknown <mohammedaich.dev@gmail.com>
2024-08-02 14:01:31 +0100

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          purple: "hsl(259, 100%, 65%)",
+          "light-red": "hsl(0, 100%, 67%)",
+        },
+        neutral: {
+          white: "hsl(0, 0%, 100%)",
+          "off-white": "hsl(0, 0%, 94%)",
+          "light-grey": "hsl(0, 0%, 86%)",
+          "smokey-grey": "hsl(0, 1%, 44%)",
+          "off-black": "hsl(0, 0%, 8%)",
+        },
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
